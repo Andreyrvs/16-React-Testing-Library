0c2389f6cb2c9814ce216b2f0d1c3a600c2da961
Andreyrvs <cortaesai@gmail.com>
2022-01-28 19:38:11 -0300

    Requisito: 02 - heading h2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import About from '../components/About';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Teste se a página contém as informações sobre a Pokédex.', () => {
+  test('a página contem heading h2 com texto "About Pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const headingH2 = screen.getByRole(
+      'heading',
+      { level: 2 },
+      { name: 'About Pokédex.' },
+    );
+    expect(headingH2).toBeInTheDocument();
+  });
+});
