61cd7338d19fa28f66efa9ebb28ef7989cfa6eab
Andreyrvs <cortaesai@gmail.com>
2022-01-28 19:30:24 -0300

    Requisito: 01 - link Home

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,18 @@
-test('testa eu', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './RenderWithRouter';
+
+describe('se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  test('O 1° link possui o texto "Home" e se redeniza a página inicial no URL "/"',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const linkHome = screen.getByRole('link', { name: 'Home' });
+      expect(linkHome).toBeInTheDocument();
+
+      userEvent.click(linkHome);
+      const { pathname } = history.location;
+      expect(pathname).toBe('/');
+    });
+});
