e64a40819bcf39881b751ec2ec922f4f1b522529
Andreyrvs <cortaesai@gmail.com>
2022-02-01 18:31:57 -0300

    Requisito: 03 - not found

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Testa se tem ou não Pokemons favoritos', () => {
+  test(`se é exibido na tela a msg "No favorite pokemon found",
+  se não tiver pokemon favorito`, () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFoundMsg = screen.getByText(/No favorite pokemon found/i);
+    expect(notFoundMsg).toBeInTheDocument();
+  });
+});
