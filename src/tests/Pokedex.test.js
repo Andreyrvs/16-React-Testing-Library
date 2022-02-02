b9430555df37a21d38f79a8c141bd203baeeb286
Andreyrvs <cortaesai@gmail.com>
2022-02-01 22:13:15 -0300

    Requisito: 05 - inpompleto

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  fail('Incompleto');
+  test('Se página contém um heading h2 com o texto Encountered pokémons.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+
+    const tagH2 = screen.getByRole(
+      'heading', { level: 2, name: /encountered pokémons/i },
+    );
+
+    expect(tagH2).toBeInTheDocument();
+  });
+  test('Se é exibido o próximo Pokémon da lista quando o botão Próximo pokémon é clicado',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/');
+
+      const btnNextPokemon = screen.getByRole('button', { name: /próximo pokémon/i });
+      expect(btnNextPokemon).toBeInTheDocument();
+      userEvent.click(btnNextPokemon);
+    });
+});
