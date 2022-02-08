01163f3db6ce0d0230d605e8bd759f5150c0c60a
Andreyrvs <cortaesai@gmail.com>
2022-02-08 12:19:04 -0300

    Requisito: 05 - falta implementar se ao recarregar a pagina o filtro all e selecionao

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -77,42 +77,92 @@ describe('Teste o componente <Pokedex.js />', () => {
     const btnOfFilter = screen.getAllByTestId(/pokemon-type-button/);
     expect(btnOfFilter).toHaveLength(BTN_FILTER_QTY);
 
-    const btnAll = screen.getByRole('button', { name: /all/i });
-    expect(btnAll).toBeVisible();
+    const btnNextPokemon = screen.getByRole('button', { name: /próximo pokémon/i });
 
     const btnElectric = screen.getByRole('button', { name: /electric/i });
     expect(btnElectric).toBeInTheDocument();
+    userEvent.click(btnElectric);
+    expect(btnNextPokemon).toBeDisabled();
+    const pikachu = screen.getByText(/pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+    const typeElectric = screen.getAllByText(/Electric/i);
+    expect(typeElectric).toHaveLength(2);
 
     const btnFire = screen.getByRole('button', { name: /fire/i });
     expect(btnFire).toBeInTheDocument();
+    userEvent.click(btnFire);
+    const charmander = screen.getByText(/charmander/i);
+    expect(charmander).toBeInTheDocument();
+    userEvent.click(btnNextPokemon);
+    const rapidash = screen.getByText(/rapidash/i);
+    expect(rapidash).toBeInTheDocument();
+    userEvent.click(btnNextPokemon);
+    expect(charmander).toBeInTheDocument();
+    const typeFire = screen.getAllByText(/Fire/i);
+    expect(typeFire).toHaveLength(2);
 
     const btnBug = screen.getByRole('button', { name: /Bug/i });
     expect(btnBug).toBeInTheDocument();
+    userEvent.click(btnBug);
+    const caterpie = screen.getByText(/caterpie/i);
+    expect(caterpie).toBeInTheDocument();
+    expect(btnNextPokemon).toBeDisabled();
+    const typeBug = screen.getAllByText(/Bug/i);
+    expect(typeBug).toHaveLength(2);
 
     const btnPoison = screen.getByRole('button', { name: /Poison/i });
     expect(btnPoison).toBeInTheDocument();
+    userEvent.click(btnPoison);
+    const ekans = screen.getByText(/ekans/i);
+    expect(ekans).toBeInTheDocument();
+    expect(btnNextPokemon).toBeDisabled();
+    const typePoison = screen.getAllByText(/Poison/i);
+    expect(typePoison).toHaveLength(2);
 
     const btnPsychic = screen.getByRole('button', { name: /Psychic/i });
     expect(btnPsychic).toBeInTheDocument();
+    userEvent.click(btnPsychic);
+    const alakazam = screen.getByText(/alakazam/i);
+    expect(alakazam).toBeInTheDocument();
+    userEvent.click(btnNextPokemon);
+    const mew = screen.getByText(/mew/i);
+    expect(mew).toBeInTheDocument();
+    userEvent.click(btnNextPokemon);
+    expect(alakazam).toBeInTheDocument();
+    const typePsychic = screen.getAllByText(/Psychic/i);
+    expect(typePsychic).toHaveLength(2);
 
     const btnNormal = screen.getByRole('button', { name: /Normal/i });
     expect(btnNormal).toBeInTheDocument();
+    userEvent.click(btnNormal);
+    const snorlax = screen.getByText(/snorlax/i);
+    expect(snorlax).toBeInTheDocument();
+    expect(btnNextPokemon).toBeDisabled();
+    const typeNormal = screen.getAllByText(/Normal/i);
+    expect(typeNormal).toHaveLength(2);
 
     const btnDragon = screen.getByRole('button', { name: /Dragon/i });
     expect(btnDragon).toBeInTheDocument();
+    userEvent.click(btnDragon);
+    const dragonair = screen.getByText(/dragonair/i);
+    expect(dragonair).toBeInTheDocument();
+    expect(btnNextPokemon).toBeDisabled();
+    const typeDragon = screen.getAllByText(/Dragon/i);
+    expect(typeDragon).toHaveLength(3);
 
-    // userEvent.click(btnFire);
-    // const typePokemon = screen.getByText(/fire/i);
-    // expect(typePokemon).toBeInTheDocument();
+    const btnAll = screen.getByRole('button', { name: /all/i });
+    userEvent.click(btnAll);
+    expect(btnAll).toBeInTheDocument();
+    expect(btnAll).toBeVisible();
   });
-  test('Se a Pokédex contém um botão para resetar o filtro', () => {
+
+  test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
     const { history } = renderWithRouter(<App />);
     history.push('/');
 
     const btnAll = screen.getByRole('button', { name: /all/i });
-    userEvent.click(btnAll);
-
-    expect(btnAll);
+    expect(btnAll).toBeInTheDocument();
+    expect(btnAll).toBeVisible();
 
     const btnNextPokemon = screen.getByRole('button', { name: /próximo pokémon/i });
     expect(btnNextPokemon).toBeInTheDocument();
