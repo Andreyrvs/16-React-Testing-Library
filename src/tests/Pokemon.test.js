6ed2a9a949814915cb95d6a1d6b4670673c5e116
Andreyrvs <cortaesai@gmail.com>
2022-02-08 13:38:42 -0300

    Requisito: 06 completo

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './RenderWithRouter';
 
@@ -16,12 +17,48 @@ describe('Teste o componente <Pokemon.js />', () => {
         renderWithRouter(<App />);
 
         const pokemonType = screen.getByTestId('pokemon-type');
+        expect(pokemonType).toHaveTextContent('Electric');
         expect(pokemonType).toBeInTheDocument();
       });
       test('O peso médio do pokémon deve ser exibido ', () => {
         renderWithRouter(<App />);
         const pokemonAverage = screen.getByTestId('pokemon-weight');
-        expect(pokemonAverage).toHaveValue('6.0');
+        expect(pokemonAverage).toHaveTextContent('Average weight: 6.0 kg');
+      });
+      test('A imagem do Pokémon deve ser exibida. ', () => {
+        renderWithRouter(<App />);
+        const srcPicachu = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+        const img = screen.getByRole('img', { name: /pikachu sprite/i });
+        expect(img.src).toBe(srcPicachu);
+        expect(img.alt).toBe('Pikachu sprite');
+      });
+      test('O card do Pokémon indicado na Pokédex contém um link de navegação ', () => {
+        const { history } = renderWithRouter(<App />);
+        const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
+        expect(linkMoreDetails).toBeInTheDocument();
+        userEvent.click(linkMoreDetails);
+        expect(history.location.pathname).toBe('/pokemons/25');
+      });
+      test('ao clicar no link de navegação do Pokémon,', () => {
+        const { history } = renderWithRouter(<App />);
+        const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
+        expect(linkMoreDetails).toBeInTheDocument();
+        userEvent.click(linkMoreDetails);
+        expect(history.location.pathname).toBe('/pokemons/25');
+        const pikachuDetails = screen.getByRole('heading', { level: 2,
+          name: /pikachu details/i });
+        expect(pikachuDetails).toBeInTheDocument();
+      });
+      test('existe um ícone de estrela nos Pokémons favoritados.', () => {
+        renderWithRouter(<App />);
+        const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
+        expect(linkMoreDetails).toBeInTheDocument();
+        userEvent.click(linkMoreDetails);
+        const labelText = screen.getByLabelText(/pokémon favoritado\?/i);
+        userEvent.click(labelText);
+        const labelStar = screen.getByRole('img', {
+          name: /Pikachu is marked as favorite/i });
+        expect(labelStar).toBeInTheDocument();
       });
     });
 });
